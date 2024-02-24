0db8dd2189061402801a90060983a2f50157863e
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2024-02-24 16:50:27 +0100

    feat: added front-end source code

---
+++ frontend/uploadclient/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {Dashboard} from '@uppy/vue'
+</script>
+
+<template>
+  <div id="app">
+    <Dashboard
+        :uppy="uppy1"
+        :plugins="[]"
+    />
+  </div>
+</template>
+
+<script>
+import Uppy from '@uppy/core'
+import Tus from '@uppy/tus'
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  computed: {
+    uppy1: () =>
+        new Uppy({
+          id: 'uppy1',
+          autoProceed: false,
+          restrictions: {
+            maxNumberOfFiles: 2,
+            minNumberOfFiles: 1
+          },
+        })
+            .use(Tus, {
+              endpoint: "http://localhost:5034/upload/",
+              headers: {
+                "Authorization": "Bearer TOKEN_VALUE"
+              },
+            }),
+  }
+})
+</script>
+
+<style src="@uppy/core/dist/style.min.css"></style>
+<style src="@uppy/dashboard/dist/style.min.css"></style>
+<style src="@uppy/drag-drop/dist/style.min.css"></style>
